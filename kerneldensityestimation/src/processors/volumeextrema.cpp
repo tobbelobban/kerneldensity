b0a7f93b1ef8a0b1c8c13665506f4febb18094dc
Tino Weinkauf <weinkauf@kth.se>
2022-02-09 14:18:32 +0100

    VolumeExtrema: Safe return when input data is not float. Only compute physical position for extrema.

--- kerneldensityestimation/src/processors/volumeextrema.cpp
+++ kerneldensityestimation/src/processors/volumeextrema.cpp
@@ -178,37 +178,40 @@ int VolumeExtrema::extreme_value_check_N6(const size_t index,
 
 void VolumeExtrema::process() {
     // input volume pointer
+    // NOTE! assumption: input volume contains single-precision floats
     const std::shared_ptr<const Volume> in_v_ptr = volume_in_.getData();
-    // get properties
-    const bool want_minima = select_minima.get();
-    const bool want_maxima = select_maxima.get();
-    const bool use_N26 = select_use_N26.get();
-    use_abs = select_use_abs.get();
+    if (in_v_ptr->getDataFormat()->getId() != DataFormatId::Float32) return;
+
+    // get access to raw input data
+    const VolumeRAM* v_ram = in_v_ptr->getRepresentation<VolumeRAM>();
+    const float* in_v_raw_ptr = static_cast<const float*>(v_ram->getData());
+
     // get input volume properties
     const auto world_matrix = in_v_ptr->getCoordinateTransformer().getIndexToWorldMatrix();
     const vec3 offset = in_v_ptr->getOffset();
     const auto basis = in_v_ptr->getBasis();
     const size3_t vol_dims = in_v_ptr->getDimensions();
     const size_t vol_size = vol_dims.x * vol_dims.y * vol_dims.z;
-    // get access to raw input data
-    const VolumeRAM* v_ram = in_v_ptr->getRepresentation<VolumeRAM>();
-    // NOTE! assumption: input volume contains single-precision floats
-    const float* in_v_raw_ptr = static_cast<const float*>(v_ram->getData());
+
+    // get user options
+    const bool want_minima = select_minima.get();
+    const bool want_maxima = select_maxima.get();
+    const bool use_N26 = select_use_N26.get();
+    use_abs = select_use_abs.get();
+
     // init buffers for extrema
     const int init_buff_sz = vol_size * 0.005f; // 0.5 % of total volume size
     std::vector<vec3> pos_v;
     std::vector<int> index_v;
     pos_v.reserve(init_buff_sz);
     index_v.reserve(init_buff_sz);
+
     // iterate over input volume and find extrema
     size_t index = 0, num_extrema = 0;
     for (int iz = 0; iz < vol_dims.z; ++iz) {
         for (int iy = 0; iy < vol_dims.y; ++iy) {
             for (int ix = 0; ix < vol_dims.x; ++ix) {
-                // get current vertex real world position
-                vec3 physical_pos = offset + vec3(ix, iy, iz) * vec3(basis[0][0] / (vol_dims.x - 1),
-                    basis[1][1] / (vol_dims.y - 1),
-                    basis[2][2] / (vol_dims.z - 1));
+
                 int res = 0;
                 if (use_N26) {
                     res = extreme_value_check_N26(index, { ix,iy,iz }, vol_dims, in_v_raw_ptr);
@@ -216,9 +219,15 @@ void VolumeExtrema::process() {
                 else {
                     res = extreme_value_check_N6(index, { ix,iy,iz }, vol_dims, in_v_raw_ptr);
                 }
+
+                if (res == 0) continue; //no extremum here
+
+                // get current vertex real world position
+                vec3 physical_pos = offset + vec3(ix, iy, iz) * vec3(basis[0][0] / (vol_dims.x - 1),
+                    basis[1][1] / (vol_dims.y - 1),
+                    basis[2][2] / (vol_dims.z - 1));
+
                 switch (res) {
-                case 0:		// not extrema
-                    break;
                 case -1:	// minimum						
                     if (want_minima) {
                         pos_v.push_back(physical_pos);
@@ -238,15 +247,18 @@ void VolumeExtrema::process() {
             }
         }
     }
+
     // print number of extrema
     LogProcessorInfo("Number of extrema: ");
     LogProcessorInfo(num_extrema);
+
     // create buffers for mesh
     auto pos_buffer = std::make_shared<Buffer<vec3>>(std::make_shared<BufferRAMPrecision<vec3>>(pos_v));
     auto index_buffer = std::make_shared<Buffer<int>>(std::make_shared<BufferRAMPrecision<int>>(index_v));
     auto mesh = std::make_shared<Mesh>(DrawType::Points, ConnectivityType::None);
     mesh->addBuffer(BufferType::PositionAttrib, pos_buffer);
     mesh->addBuffer(BufferType::IndexAttrib, index_buffer);
+
     // set mesh ptr on outport
     mesh_out_.setData(mesh);
 }
